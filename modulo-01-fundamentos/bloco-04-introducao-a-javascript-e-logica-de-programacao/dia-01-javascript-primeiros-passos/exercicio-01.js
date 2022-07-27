584e2a4bde2a8ee0350297b46ac74f388543fb9e
flavioramonnunes <f.ramon_paixao@hotmail.com>
2022-07-27 11:20:49 -0300

    Exercício de Operadores Aritméticos.

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-01.js
@@ -0,0 +1,8 @@
+const a = 5;
+const b = 3;
+
+console.log("Const a = " + a + " e const b = " + b + ". O resultado de a + b é " + (a+b) + ".");
+console.log("Const a = " + a + " e const b = " + b + ". O resultado de a - b é " + (a-b) + ".");
+console.log("Const a = " + a + " e const b = " + b + ". O resultado de a * b é " + (a*b) + ".");
+console.log("Const a = " + a + " e const b = " + b + ". O resultado de a / b é " + (a/b) + ".");
+console.log("Const a = " + a + " e const b = " + b + ". O resultado de a % b é " + (a%b) + ".");
