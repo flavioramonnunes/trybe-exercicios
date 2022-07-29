3e2f301bd2517dde7021179c3fcb58ea66ad945b
flavioramonnunes <f.ramon_paixao@hotmail.com>
2022-07-29 09:22:01 -0300

    Exercício 09.

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-09.js
@@ -0,0 +1,5 @@
+//Utilizando o array criado no exercício anterior (exercicio-08.js) imprima o resultado da divisão de cada um dos elementos por 2.
+
+for (let number = 1; number <= 25; number++) {
+    console.log(number / 2);
+}
