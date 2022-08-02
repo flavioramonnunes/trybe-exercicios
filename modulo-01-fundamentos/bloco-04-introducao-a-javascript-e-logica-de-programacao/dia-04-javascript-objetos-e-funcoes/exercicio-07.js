cbc757121619f39665cc9ac65dd5aacaaa12f11f
flavioramonnunes <f.ramon_paixao@hotmail.com>
2022-08-02 17:38:47 -0300

    Exercício 07.

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-07.js
@@ -0,0 +1,19 @@
+//Adicione um novo livro favorito na chave livrosFavoritos, que é um array.
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+        {
+            titulo: 'Bíblia Sagrada',
+            autor: 'Vários escritores',
+            editora: 'Vida',
+        }
+    ],
+};
